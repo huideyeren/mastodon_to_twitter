59218c22d1ba285611fa222551fc81e62874ff88
Takakura Yusuke <takakura.yusuke@gmail.com>
2017-05-24 08:04:35 +0900

    Test Tweet and Toot.

--- mas2twi.py
+++ mas2twi.py
@@ -1,32 +1,36 @@
+import settings
 from mastodon import *
-from tweepy import *
-from dotenv import load_dotenv
+import tweepy
 
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+TWITTER_CONSUMER_KEY = settings.twi_con_key
+TWITTER_CONSUMER_SECRET = settings.twi_con_seq
+TWITTER_ACCESS_TOKEN = settings.twi_acc_tkn
+TWITTER_ACCESS_SECRET = settings.twi_acc_seq
 
-class TootConfig(object):
-    """Configure Mastodon and Twitter."""
-
-    def login_for_mastodon(self):
+class Mas2twiAuth:
+    def login_for_mastodon():
         mastodon = Mastodon(
             client_id="my_clientcred_nico.txt",
             access_token="my_usercred_nico.txt",
             api_base_url="https://friends.nico")
         return mastodon
 
-    def login_for_twitter(self):
-        auth = tweepy.OAuthHandler(
-            TWITTER_CONSUMER_KEY,
-            TWITTER_CONSUMER_SECRET)
-        auth.set_access_token(
-            TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+
+    def login_for_twitter():
+        auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+        auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
         twitter = tweepy.API(auth)
         return twitter
 
-tweet = TootConfig().login_for_twitter()
+tweet = "Pythonから試しにマストドンとTwitterに書き込んでみる。"
 
-public_tweets = tweet.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+mstdn = Mas2twiAuth.login_for_mastodon()
+
+twi = Mas2twiAuth.login_for_twitter()
+
+twi.update_status(tweet)
+mstdn.toot(tweet)
+
+
+
